ee71ab74ec092cc24830b106b852481b1a9aaefc
Amro <2592997@students.wits.ac.za>
2025-03-05 18:09:33 +0200

    Working On Tiles

--- Local Multiplayer - FPS GAME/Assets/Scripts/Game Scripts/Tiles/TileController.cs
+++ Local Multiplayer - FPS GAME/Assets/Scripts/Game Scripts/Tiles/TileController.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class TileController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject TestTile;
+    public float moveSpeed = 5f;  
+
+    private Vector3 targetScale;
+    private Vector3 targetPosition;
+
+    private void Start()
     {
-        
+        targetScale = TestTile.transform.localScale;
+        targetPosition = TestTile.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))  
+        {
+            targetScale.z = TestTile.transform.localScale.z + 250;
+
+            targetPosition = new Vector3(TestTile.transform.position.x, targetScale.z / 1166.940397f, TestTile.transform.position.z);
+        }
+
+        TestTile.transform.localScale = Vector3.Lerp(TestTile.transform.localScale, targetScale, moveSpeed * Time.deltaTime);
+        TestTile.transform.position = Vector3.Lerp(TestTile.transform.position, targetPosition, moveSpeed * Time.deltaTime);
     }
 }
