5d808e259c17b47173e31962a0ef7f0fda79b0fb
Amro <2592997@students.wits.ac.za>
2025-03-05 17:43:37 +0200

    Adjusted Spawn Height

--- Local Multiplayer - FPS GAME/Assets/Scripts/Game Scripts/Map Generation/MapController.cs
+++ Local Multiplayer - FPS GAME/Assets/Scripts/Game Scripts/Map Generation/MapController.cs
@@ -18,13 +18,17 @@ public class MapController : MonoBehaviour
 
     public void GenerateMap()
     {
-        for (int i = 0; i < 9; i++)
+        for (int i = 0; i < 15; i++)
         {
             int randomTile = Random.Range(0, Tiles.Length);
             int randomNumber = Random.Range(0, 40) * 50;
             Vector3 newScale = new Vector3(100, 100, randomNumber);
-            Tiles[randomTile].transform.localScale = newScale; 
+            Tiles[randomTile].transform.localScale = newScale;
 
+            Vector3 newPosition = new Vector3(Tiles[randomTile].transform.position.x, Tiles[randomTile].transform.localScale.z / 1166.940397f, Tiles[randomTile].transform.position.z);
+            Tiles[randomTile].transform.position = newPosition;
+
+            //divide height by this 1158.940397
         }
     }
 }
