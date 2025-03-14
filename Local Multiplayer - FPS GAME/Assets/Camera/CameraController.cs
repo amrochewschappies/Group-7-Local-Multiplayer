4fb2ba6bc6d7c5c8866bf8a5c86889fa93afc7be
Amro <2592997@students.wits.ac.za>
2025-03-14 07:41:05 +0200

    Added Dolly Camera

--- Local Multiplayer - FPS GAME/Assets/Camera/CameraController.cs
+++ Local Multiplayer - FPS GAME/Assets/Camera/CameraController.cs
@@ -4,13 +4,11 @@ public class CameraController : MonoBehaviour
 {
     public Camera PlayerOneCamera;
     public Camera PlayerTwoCamera;
-    public Camera OtherPlayerTwoCamera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         PlayerOneCamera.rect = new Rect(0, 0.5f, 1, 0.5f);
         PlayerTwoCamera.rect = new Rect(0, 0, 1, 0.5f);
-        OtherPlayerTwoCamera.rect = new Rect(0, 0, 1, 0.5f);
     }
 
     // Update is called once per frame
